7d0253f4234fada4850ef8af82752147d6fec6b4
bradleyerickson <bradley.erickson25@gmail.com>
2015-02-17 13:23:56 -0500

    Changing over to templates

--- generator.py
+++ generator.py
@@ -1,16 +1,16 @@
 # Main bottle route generator 
 
-from bottle import route, run, static_file, Bottle
+from bottle import route, run, Bottle, template
 
 main = Bottle()
 
 @main.route('/')
 def index():
-	return static_file('main.html', root='/home/brad/Documents/Bottle/WebApi/RoboticsWebApi')
-	
+	return template('main_temp')
+
 @main.route('/<direction>')
 def direction(direction):
 	print(direction)
-	return static_file('main.html', root='/home/brad/Documents/Bottle/WebApi/RoboticsWebApi')
+	return direction
 	
 run(main, host='localhost', port=8080)
