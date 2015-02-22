98d697848196f7e024411d55062ef7a3e1a6d856
bradleyerickson <bradley.erickson25@gmail.com>
2015-02-22 17:57:59 -0500

    Added requirements.txt and started migration of server.py into generator.py and deleted server.py

--- generator.py
+++ generator.py
@@ -1,8 +1,18 @@
 # Main bottle route generator 
 
-from bottle import route, run, Bottle, template
+from bottle import *
+from subprocess import check_output
 
 main = Bottle()
+
+	
+_COMMAND_MAP = {
+    #path (endpoint) => shell command
+    'forward':'ls',
+    'backward':'ls',
+    'left' : 'ls -a',
+    'right' : ['echo', 'bye world']
+}
 
 @main.route('/')
 def index():
@@ -10,7 +20,11 @@ def index():
 
 @main.route('/<direction>')
 def direction(direction):
-	print(direction)
-	return direction
+	command = _COMMAND_MAP[direction]
+	
+	if check_output(command, shell=True):
+		return 'Success'
+	else:
+		return 'Fail'
 	
 run(main, host='localhost', port=8080)
